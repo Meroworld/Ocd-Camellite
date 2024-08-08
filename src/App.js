ca1365ba47bb05e86857f5d588f0e923a1507a2c
Meroworld <domerocollins@gmail.com>
2024-08-08 06:50:21 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>OCD CAMELLITE</h1>
+      <h3>Under Construction</h3>
+     </div>
+  );
+}
+
+export default App;
